b3657098151a60e432e0416ed0b10b21bbd6a9c8
shimeming <shimeming@gmail.com>
2025-12-07 01:49:48 +0800

    add mongo db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,5 @@
 import pgPromise from 'pg-promise';
+import { MongoClient } from 'mongodb';
 import { loadEnv } from './env.js';
 
 loadEnv();
@@ -11,3 +12,24 @@ export const db = pgp({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
 });
+
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const mongoPort = process.env.MONGO_PORT || '27017';
+const mongoDbName = process.env.MONGO_DB_NAME || 'jojo';
+
+const mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/?authSource=admin`;
+
+export const mongoClient = new MongoClient(mongoUrl);
+export const mongoDb = mongoClient.db(mongoDbName);
+
+export const connectMongo = async () => {
+  try {
+    await mongoClient.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+  }
+};
